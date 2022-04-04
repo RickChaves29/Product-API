4349d4afb23d18d6a8188e4c771eaa1f850b8214
Henrique chaves <canaldomonza@gmail.com>
2022-04-04 19:02:29 -0300

    create entities

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -1,1 +1,8 @@
-export class CreateProductDto {}
+export class CreateProductDto {
+  name: string;
+  description: string;
+  quantity: number;
+  price: number;
+  createdAt: Date;
+  updatedAt: Date;
+}
--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -1,1 +1,9 @@
-export class Product {}
+export class Product {
+  id: string;
+  name: string;
+  description: string;
+  quantity: number;
+  price: number;
+  createdAt: Date;
+  updatedAt: Date;
+}
